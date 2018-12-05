8fea16b5017572d4c773e3c234ecb9b04ba9234b
Arya Kumar <2019akumar2@tjhsst.edu>
2018-12-04 22:45:04 -0500

    cleaned up soln.py with better step function

--- soln.py
+++ soln.py
@@ -1,12 +1,19 @@
+"""
+soln.py
+
+Utility script used to compute the optimal
+reward for a single episode, using the same
+reward function we use for training the HoverEnv
+"""
+
 from crazyenv.env import HoverEnv
 
 env = HoverEnv()
-
-obs = env.reset()
 total_reward = 0.0
 
-for _ in range(512):
-    obs, reward, done, _ = env.step([0., 0., 1-obs[2]])
+obs = env.reset()
+for _ in range(256):
+    obs, reward, done, _ = env.step([0,0,1] - obs)
     total_reward += reward
 
 print(total_reward)
