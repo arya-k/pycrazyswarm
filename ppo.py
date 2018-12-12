1c65c7d9cbf8dac1130c3fdb224e6e91b62d6df4
Arya Kumar <2019akumar2@tjhsst.edu>
2018-12-12 11:09:53 -0500

    refactored the PPO code to use multithreading, PPO-clip

--- ppo.py
+++ ppo.py
@@ -1,193 +1,262 @@
 #!/usr/bin/env python3
-import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-import gym
 
-from crazyenv.env import HoverEnv
+"""
+This ppo.py file contains the main PPO class, and 
+util functions to save / load the class. It also
+contains functions to train the PPO class in a multi-
+threaded fashion.
+"""
+
+import threading
+import queue
+import os
+import cloudpickle
+import numpy as np
+import tensorflow as tf
 from time import time
 
-EP_MAX = 5000
-EP_LEN = 256
-GAMMA = 0.98
-A_LR = 0.0001
-C_LR = 0.0002
-BATCH = 64
-A_UPDATE_STEPS = 10
-C_UPDATE_STEPS = 10
-S_DIM, A_DIM = 3, 3
-METHOD = [
-    dict(name='kl_pen', kl_target=0.01, lam=0.5),   # KL penalty
-    dict(name='clip', epsilon=0.2),                 # Clipped surrogate objective, find this is better
-][0]        # choose the method for optimization
+class Global():
 
-
-class PPO(object):
+    ''' Globals as a shared object, which can be serialized. '''
 
     def __init__(self):
-        self.sess = tf.Session()
-        self.tfs = tf.placeholder(tf.float32, [None, S_DIM], 'state')
+        self.UPDATE_EVENT = threading.Event()
+        self.ROLLING_EVENT = threading.Event()
+        self.GLOBAL_UPDATE_COUNTER = 0
+        self.GLOBAL_EP = 0
+        self.GLOBAL_RUNNING_R = []
+        self.COORD = tf.train.Coordinator()
+        self.QUEUE = queue.Queue() # workers put data in this queue
+        self.start_t = time()
+
+
+class PPO():
+
+    ''' Proximal Policy Optimization with Serialization and 
+    Multithreading support. Based off of OpenAI Baselines' implementation '''
+
+    def __init__(self, tr_vars):
+        ''' tr_vars: the training variables'''
+        tf.reset_default_graph()
+        self.sess = tf.Session() # create a session
+        self.g = Global()
+        self.tr_vars = tr_vars
+        self.tfs = tf.placeholder( # tensorflow state
+            tf.float32,
+            [None, tr_vars['ENV'].obs_dim],
+            'state')
 
         # critic
-        with tf.variable_scope('critic'):
-            l1 = tf.layers.dense(self.tfs, 100, tf.nn.relu)
-            self.v = tf.layers.dense(l1, 1)
-            self.tfdc_r = tf.placeholder(tf.float32, [None, 1], 'discounted_r')
-            self.advantage = self.tfdc_r - self.v
-            self.closs = tf.reduce_mean(tf.square(self.advantage))
-            self.ctrain_op = tf.train.AdamOptimizer(C_LR).minimize(self.closs)
+        l1 = tf.layers.dense(self.tfs, 100, tf.nn.relu) # first layer
+        self.v = tf.layers.dense(l1, 1) # value
+        self.tfdc_r = tf.placeholder(tf.float32, [None, 1], 'discounted_r') # discounted reward
+        self.advantage = self.tfdc_r - self.v
+        self.closs = tf.reduce_mean(tf.square(self.advantage)) # critic loss
+        self.ctrain_op = tf.train.AdamOptimizer(
+            tr_vars['C_LR']).minimize(self.closs)
 
         # actor
         pi, pi_params, mu = self._build_anet('pi', trainable=True)
         oldpi, oldpi_params, _ = self._build_anet('oldpi', trainable=False)
-        with tf.variable_scope('sample_action'):
-            self.sample_op = tf.squeeze(pi.sample(1), axis=0)       # choosing action
-        with tf.variable_scope('det_action'):
-            self.det_op = tf.squeeze(mu.sample(1), axis=0)
-        with tf.variable_scope('update_oldpi'):
-            self.update_oldpi_op = [oldp.assign(p) for p, oldp in zip(pi_params, oldpi_params)]
+        self.sample_op = tf.squeeze(pi.sample(1), axis=0) # sampling action
+        self.det_op = tf.squeeze(mu.sample(1), axis=0) # determinstic action
+        self.update_oldpi_op = [oldp.assign(p) for p, oldp in 
+                                zip(pi_params, oldpi_params)]
 
-        self.tfa = tf.placeholder(tf.float32, [None, A_DIM], 'action')
+        self.tfa = tf.placeholder(tf.float32, [None, tr_vars['ENV'].act_dim], 'action')
         self.tfadv = tf.placeholder(tf.float32, [None, 1], 'advantage')
-        with tf.variable_scope('loss'):
-            with tf.variable_scope('surrogate'):
-                # ratio = tf.exp(pi.log_prob(self.tfa) - oldpi.log_prob(self.tfa))
-                ratio = pi.prob(self.tfa) / oldpi.prob(self.tfa)
-                surr = ratio * self.tfadv
-            if METHOD['name'] == 'kl_pen':
-                self.tflam = tf.placeholder(tf.float32, None, 'lambda')
-                kl = tf.distributions.kl_divergence(oldpi, pi)
-                self.kl_mean = tf.reduce_mean(kl)
-                self.aloss = -(tf.reduce_mean(surr - self.tflam * kl))
-            else:   # clipping method, find this is better
-                self.aloss = -tf.reduce_mean(tf.minimum(
-                    surr,
-                    tf.clip_by_value(ratio, 1.-METHOD['epsilon'], 1.+METHOD['epsilon'])*self.tfadv))
+        
+        ratio = pi.prob(self.tfa) / (oldpi.prob(self.tfa) + 1e-5)
+        surr = ratio * self.tfadv # surrogate loss
 
-        with tf.variable_scope('atrain'):
-            self.atrain_op = tf.train.AdamOptimizer(A_LR).minimize(self.aloss)
+        self.aloss = -tf.reduce_mean(tf.minimum(surr, # clipped surrogate objective
+            tf.clip_by_value(ratio,
+                1. - tr_vars['EPSILON'],
+                1. + tr_vars['EPSILON']) * self.tfadv))
 
-        # tf.summary.FileWriter("log/", self.sess.graph)
+        self.atrain_op = tf.train.AdamOptimizer(
+            tr_vars['A_LR']).minimize(self.aloss)
+
+        with tf.variable_scope('model'):
+              self.params = tf.trainable_variables()
 
         self.sess.run(tf.global_variables_initializer())
 
-    def update(self, s, a, r):
-        self.sess.run(self.update_oldpi_op)
-        adv = self.sess.run(self.advantage, {self.tfs: s, self.tfdc_r: r})
-        # adv = (adv - adv.mean())/(adv.std()+1e-6)     # sometimes helpful
+    def update(self):
+        ''' Multithreaded training loop. '''
+        update_step = self.tr_vars['UPDATE_STEP']
+        s_dim = self.tr_vars['ENV'].obs_dim
+        a_dim = self.tr_vars['ENV'].act_dim
 
-        # update actor
-        if METHOD['name'] == 'kl_pen':
-            for _ in range(A_UPDATE_STEPS):
-                _, kl = self.sess.run(
-                    [self.atrain_op, self.kl_mean],
-                    {self.tfs: s, self.tfa: a, self.tfadv: adv, self.tflam: METHOD['lam']})
-                if kl > 4*METHOD['kl_target']:  # this in in google's paper
-                    break
-            if kl < METHOD['kl_target'] / 1.5:  # adaptive lambda, this is in OpenAI's paper
-                METHOD['lam'] /= 2
-            elif kl > METHOD['kl_target'] * 1.5:
-                METHOD['lam'] *= 2
-            METHOD['lam'] = np.clip(METHOD['lam'], 1e-4, 10)    # sometimes explode, this clipping is my solution
-        else:   # clipping method, find this is better (OpenAI's paper)
-            [self.sess.run(self.atrain_op, {self.tfs: s, self.tfa: a, self.tfadv: adv}) for _ in range(A_UPDATE_STEPS)]
+        while not self.g.COORD.should_stop():
+            if self.g.GLOBAL_EP < self.tr_vars['EP_MAX']:
+                self.g.UPDATE_EVENT.wait() # wait until get batch of data
+                self.sess.run(self.update_oldpi_op) # copy pi to old pi
+                data = [self.g.QUEUE.get() for _ in range(self.g.QUEUE.qsize())] # collect data from all workers
+                data = np.vstack(data)
+                s, a, r = data[:, :s_dim], data[:, s_dim: s_dim + a_dim], data[:, -1:]
+                adv = self.sess.run(self.advantage, {self.tfs: s, self.tfdc_r: r})
+                # update actor and critic in a update loop
+                [self.sess.run(self.atrain_op, {self.tfs: s, self.tfa: a, self.tfadv: adv}) for _ in range(update_step)]
+                [self.sess.run(self.ctrain_op, {self.tfs: s, self.tfdc_r: r}) for _ in range(update_step)]
+                self.g.UPDATE_EVENT.clear() # updating finished
+                self.g.GLOBAL_UPDATE_COUNTER = 0 # reset counter
+                self.g.ROLLING_EVENT.set() # set roll-out available
 
-        # update critic
-        [self.sess.run(self.ctrain_op, {self.tfs: s, self.tfdc_r: r}) for _ in range(C_UPDATE_STEPS)]
-
-    def _build_anet(self, name, trainable):
+    def _build_anet(self, name, trainable=True):
+        ''' Build the actor's network. Returns pi and mu. '''
+        a_dim = self.tr_vars['ENV'].act_dim # action dimensions
         with tf.variable_scope(name):
-            l1 = tf.layers.dense(self.tfs, 100, tf.nn.relu, trainable=trainable)
-            mu = tf.layers.dense(l1, A_DIM, tf.nn.tanh, trainable=trainable)
-            sigma = tf.layers.dense(l1, A_DIM, tf.nn.softplus, trainable=trainable)
+            l1 = tf.layers.dense(self.tfs, 200, tf.nn.relu, trainable=trainable)
+            l2 = tf.layers.dense(l1, 200, tf.nn.relu, trainable=trainable)
+            mu = tf.layers.dense(l2, a_dim, tf.nn.tanh, trainable=trainable)
+            sigma = tf.layers.dense(l2, a_dim, tf.nn.softplus, trainable=trainable)
             norm_dist = tf.distributions.Normal(loc=mu, scale=sigma)
         params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=name)
         return norm_dist, params, tf.distributions.Normal(loc=mu, scale=0.)
 
-    def choose_action(self, s):
-        s = s[np.newaxis, :]
-        a = self.sess.run(self.sample_op, {self.tfs: s})[0]
-        return a
+    def sample_action(self, s):
+        ''' Get the sampling policy's action, given a state s. '''
+        s = s[np.newaxis, :] # reshape the input
+        return self.sess.run(self.sample_op, {self.tfs: s})[0]
 
     def det_action(self, s):
-        '''Deterministic action (action not based on standard deviation, only mean)'''
-        s = s[np.newaxis, :]
-        a = self.sess.run(self.det_op, {self.tfs: s})[0]
-        return a
+        ''' Get the deterministic policy's action, given a state s. '''
+        s = s[np.newaxis, :] # reshape the input
+        return self.sess.run(self.det_op, {self.tfs: s})[0]
 
     def get_v(self, s):
-        if s.ndim < 2: s = s[np.newaxis, :]
-        return self.sess.run(self.v, {self.tfs: s})[0, 0]
+        ''' The critic method's estimate of the reward, given a state s. '''
+        if s.ndim < 2: s = s[np.newaxis, :] # reshape input if necessary
+        return self.sess.run(self.v, {self.tfs: s})[0, 0] # get critic's output
 
-env = HoverEnv()
-ppo = PPO()
-all_ep_r = []
+    def save(self, save_path):
+        data = self.tr_vars
+        params = self.sess.run(self.params)
 
-mean = [0,0,1]
-std = np.array([1,1,.5])
+        if isinstance(save_path, str):
+            _, ext = os.path.splitext(save_path)
+            if ext == "":
+                save_path += ".pkl"
+            with open(save_path, "wb") as file_:
+                cloudpickle.dump((data, params), file_)
+        else:
+            # Here save_path is a file-like object, not a path
+            cloudpickle.dump((data, params), save_path)
 
-def train_once(ep):
-    loc = np.random.normal(mean, std)
-    s = env.reset(loc)
-    buffer_s, buffer_a, buffer_r = [], [], []
-    ep_r = 0
-    for t in range(EP_LEN):    # in one episode
-        # env.render()
-        a = ppo.choose_action(s)
-        s_, r, done, _ = env.step(a)
-        buffer_s.append(s)
-        buffer_a.append(a)
-        buffer_r.append((r+8)/8)    # normalize reward, find to be useful
-        s = s_
-        ep_r += r
+    @classmethod
+    def load(cls, load_path):
+        if isinstance(load_path, str):
+            if not os.path.exists(load_path):
+                if os.path.exists(load_path + ".pkl"):
+                    load_path += ".pkl"
+                else:
+                    raise ValueError("Error: the file {} could not be found".format(load_path))
 
-        # update ppo
-        if (t+1) % BATCH == 0 or t == EP_LEN-1:
-            v_s_ = ppo.get_v(s_)
-            discounted_r = []
-            for r in buffer_r[::-1]:
-                v_s_ = r + GAMMA * v_s_
-                discounted_r.append(v_s_)
-            discounted_r.reverse()
+            with open(load_path, "rb") as file:
+                data, params = cloudpickle.load(file)
+        else:
+            # Here load_path is a file-like object, not a path
+            data, params = cloudpickle.load(load_path)
 
-            bs, ba, br = np.vstack(buffer_s), np.vstack(buffer_a), np.array(discounted_r)[:, np.newaxis]
+        model = cls(data)
+
+        restores = []
+        for param, loaded_p in zip(model.params, params):
+            restores.append(param.assign(loaded_p))
+        model.sess.run(restores)
+
+        return model
+
+
+class Worker():
+
+    ''' Multithreaded simulators, which each independently train the
+    PPO algorithm, according to it's current policies. '''
+
+    def __init__(self, algo, tr_vars):
+        ''' Creates a copy of the env, and shares algo, the PPO object. '''
+        self.ppo = algo # global, shared PPO class
+        self.env = tr_vars['ENV']() # individual copy of the env.
+        self.g = algo.g
+
+        self.ep_len = tr_vars['EP_LEN']
+        self.ep_max = tr_vars['EP_MAX']
+        self.gamma = tr_vars['GAMMA']
+        self.min_batch_size = tr_vars['MIN_BATCH_SIZE']
+
+    def work(self):
+        ''' Training loop to optimize PPO while the coordinator states that
+        training is not yet complete. Always acts according to the most up to
+        date policy.'''
+        while not self.g.COORD.should_stop():
+            s = self.env.reset()
+            ep_r = 0
             buffer_s, buffer_a, buffer_r = [], [], []
-            ppo.update(bs, ba, br)
-    if ep == 0: all_ep_r.append(ep_r)
-    else: all_ep_r.append(all_ep_r[-1]*0.9 + ep_r*0.1)
+            for t in range(self.ep_len):
+                if not self.g.ROLLING_EVENT.is_set(): # while global PPO is updating
+                    self.g.ROLLING_EVENT.wait() # wait until PPO is updated
+                    buffer_s, buffer_a, buffer_r = [], [], [] # clear history buffer, use new policy to collect data
+                a = self.ppo.sample_action(s)
+                s_, r, done, _ = self.env.step(a)
+                buffer_s.append(s)
+                buffer_a.append(a)
+                buffer_r.append((r + 8) / 8) # normalize reward, find to be useful
+                s = s_
+                ep_r += r
 
-    msg = "Ep: {0:<5}".format(ep)
-    msg += " | Ep_r: {:<8}".format("{:.3f}".format(ep_r))
-    msg += " | Lam: {:.4f}".format(METHOD['lam'])
-    return msg
+                self.g.GLOBAL_UPDATE_COUNTER += 1 # count to minimum batch size, no need to wait other workers
+                if t == self.ep_len - 1 or self.g.GLOBAL_UPDATE_COUNTER >= self.min_batch_size:
+                    v_s_ = self.ppo.get_v(s_)
+                    discounted_r = [] # compute discounted reward
+                    for r in buffer_r[::-1]:
+                        v_s_ = r + self.gamma * v_s_
+                        discounted_r.append(v_s_)
+                    discounted_r.reverse()
 
-start = time()
-for ep in range(EP_MAX):
-    msg = train_once(ep)
-    print(msg + ' | ETA: {:.0f}m {:.0f}s'.format(*divmod((time()-start) * (EP_MAX - ep - 1) / (ep + 1), 60)))
+                    bs, ba, br = np.vstack(buffer_s), np.vstack(buffer_a), np.array(discounted_r)[:, np.newaxis]
+                    buffer_s, buffer_a, buffer_r = [], [], []
+                    self.g.QUEUE.put(np.hstack((bs, ba, br))) # put data in the queue
+                    if self.g.GLOBAL_UPDATE_COUNTER >= self.min_batch_size:
+                        self.g.ROLLING_EVENT.clear() # stop collecting data
+                        self.g.UPDATE_EVENT.set() # globalPPO update
 
-print("""EP_MAX = {EP_MAX}
-EP_LEN = {EP_LEN}
-GAMMA = {GAMMA}
-A_LR = {A_LR}
-C_LR = {C_LR}
-BATCH = {BATCH}
-A_UPDATE_STEPS = {A_UPDATE_STEPS}
-C_UPDATE_STEPS = {C_UPDATE_STEPS}
-TRAINING TIME: {delta_t:.3f}s""".format(delta_t=time()-start, **globals()))
+                    if self.g.GLOBAL_EP >= self.ep_max: # stop training
+                        self.g.COORD.request_stop()
+                        break
 
-plt.plot(np.arange(len(all_ep_r)), all_ep_r)
-plt.xlabel('Episode')
-plt.ylabel('Moving averaged episode reward')
-plt.show()
+            # record reward changes, plot later
+            if len(self.g.GLOBAL_RUNNING_R) == 0:
+                self.g.GLOBAL_RUNNING_R.append(ep_r)
+            else:
+                self.g.GLOBAL_RUNNING_R.append(self.g.GLOBAL_RUNNING_R[-1]*0.9+ep_r*0.1)
 
-while True:
-    s = env.reset(np.random.normal([0,0,0], [0.01, 0.01, 0]))
-    total_reward = 0.
-    for t in range(512):
-        a = ppo.det_action(s)
-        s, r, *_ = env.step(a)
-        total_reward += r
-        env.render()
-    print("total reward: {}".format(total_reward))
+            print('EP: {} | EP_R: {:.3f} | ETA: {:.0f}m {:.0f}s'.format(
+                self.g.GLOBAL_EP, self.g.GLOBAL_RUNNING_R[-1],
+                *divmod((time()-self.g.start_t)*(self.ep_max-self.g.GLOBAL_EP-1) /
+                    (self.g.GLOBAL_EP + 1), 60))) # VERY ugly print statement ¯\_(ツ)_/¯
+
+            self.g.GLOBAL_EP += 1
+
+
+def train_multithread(algo, training_vars, num_workers):
+    ''' Train algo, a PPO object, using multithreaded workers. '''
+    algo.g.UPDATE_EVENT.clear() # not update now
+    algo.g.ROLLING_EVENT.set() # start to roll out
+    workers = [Worker(algo, training_vars) for _ in range(num_workers)]
+    
+    threads = []
+    for worker in workers: # worker threads
+        t = threading.Thread(target=worker.work, args=())
+        t.start() # training
+        threads.append(t)
+
+    # add a PPO updating thread
+    threads.append(threading.Thread(target=algo.update,))
+    threads[-1].start()
+    algo.g.COORD.join(threads)
+
+    return algo.g.GLOBAL_RUNNING_R
+
+
 
