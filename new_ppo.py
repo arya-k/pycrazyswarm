2b628741ddd76a0c645a215b7ee44bef9873ee0b
arya-k <thearyaskumar@icloud.com>
2019-02-14 09:49:49 -0500

    training ppo on StaticObstEnv

--- new_ppo.py
+++ new_ppo.py
@@ -2,23 +2,43 @@ import gym
 import tensorflow as tf
 
 from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
 from stable_baselines import PPO2
 
-env = DummyVecEnv([lambda: gym.make('CartPole-v1')])
+from crazyenv.env2 import StaticObstEnv
+from crazyenv.env import HoverEnv
 
-policy_kwargs = dict(act_fun=tf.nn.tanh, net_arch=[32, 32])
+env = DummyVecEnv([lambda: StaticObstEnv()])
 
-model = PPO2(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, verbose=1)
-model.learn(total_timesteps=200000, log_interval=5)
+policy_kwargs = dict(act_fun=tf.nn.tanh, net_arch=[200,200])
 
-# model.save("ppo2_cartpole")
+model = PPO2(
+    MlpPolicy, env,
+    gamma=0.98,
+    n_steps=256,
+    policy_kwargs=policy_kwargs,
+    nminibatches=64,
+    learning_rate=1e-4,
+    cliprange=0.2,
+    tensorboard_log='/home/um/tensorboards/',
+    verbose=1
+)
+
+model.learn(
+    total_timesteps=200000,
+    log_interval=1
+)
+
+model.save("ppo2_staticobst")
 # del model # remove to demonstrate saving and loading
 # model = PPO2.load("ppo2_cartpole")
 
 # Enjoy trained agent
-obs = env.reset()
 while True:
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    env.render()
+    obs = env.reset()
+    done = False
+    while not done:
+        action, _states = model.predict(obs, deterministic=False)
+        obs, rewards, done, info = env.step(action)
+        env.render()
+
